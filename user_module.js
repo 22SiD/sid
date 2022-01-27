bed7047b6c61920d04160175043535d96c53ac09
22SiD <78725482+22SiD@users.noreply.github.com>
2022-01-27 13:04:44 +0530

    Add files via upload

---
+++ user_module.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const conn_str = "mongodb+srv://sid:root@cluster0.xrrg4.mongodb.net/tcet?retryWrites=true&w=majority"
+
+mongoose.connect(conn_str, { useNewUrlParser: true , useUnifiedTopology: true})
+    .then( () => console.log("Connected successfully...") )
+    .catch( (err) => console.log(err) );
+
+const orderSchema = new mongoose.Schema({
+  "name": String,
+  "age": String,
+  "city": String
+      }
+  
+);
+
+const orderModel = new mongoose.model("users", orderSchema);
+
+exports.order = orderModel;
